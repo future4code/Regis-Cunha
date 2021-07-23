097fd34b9a7c7a6026929a8691049332d9986551
RegisAlencar <regis.alencar.cunha@gmail.com>
2021-07-22 21:25:40 -0300

    strings-arrays

---
+++ quinzena1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,76 @@
+
+//Exercícios de interpretação de código:
+// Exercicio 1:
+
+// /a) imprimiu uma array indefinida
+//b) transformou o array em null
+//c) o comando length retornará o comprimento do array
+//d) transformou o "i" em 0, por fim, imprimiu a propriedade "i"
+//e) somou a propriedade "i"+1 resultando em 19
+//f) selecionou a propriedade 6 depois do "i", sendo o 10.
+
+// Exercicio 2:
+// A frase depois dos comandos mencionados ficará:
+// "SUBI NUM ÔNIBUS EM MIRROCOS", 27
+
+// Exercícios de escrita de código:
+// 1)
+
+//  Let = nomeDoUsuario = prompt("Qual é o seu nome?")
+//  let = emailDoUsuario = prompt("Qual é o seu email?")
+
+//  console.log("O e-mail", emailDoUsuario, "foi cadastrado com sucesso. Seja bem-vinda(o),", nomeDoUsuario)
+
+//  2)
+
+// const frutasPreferidas = ["maça", "manga", "melancia", "uva", "abacaxi"]
+// console.log(frutasPreferidas)
+
+// console.log("Essas são as minhas frutas preferidas:")
+// console.log(frutasPreferidas[0])
+// console.log(frutasPreferidas[1])
+// console.log(frutasPreferidas[2])
+// console.log(frutasPreferidas[3])
+// console.log(frutasPreferidas[4])
+
+// const comidaPreferidaUsuario = prompt("Qual a sua comida preferida")
+
+// const novaLista = frutasPreferidas
+// console.log("A nova lista de comidas preferidas são:", novaLista[0])
+// console.log(comidaPreferidaUsuario)
+// console.log(novaLista[2])
+// console.log(novaLista[3])
+// console.log(novaLista[4])
+
+//3)
+
+// const listaDeTarefas = ["0", "1", "2"]
+// const listaUsuario = prompt("Digite uma tarefa pendente.")
+// const listaUsuario1 = prompt("Digite outra tarefa pendente.")
+// const listaUsuario2 = prompt("Digite mais uma tarefa pendente.")
+
+// console.log(listaUsuario, listaUsuario1, listaUsuario2)
+
+// resultadoUsuario = prompt("Digite a tarefa ja realizada. Ex: 0, 1 ou 2")
+
+// console.log(listaDeTarefas[resultadoUsuario - 1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
