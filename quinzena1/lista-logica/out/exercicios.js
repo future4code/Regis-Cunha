adf2dd0d39121e881ce0cb3d4c5df6612080cb6c
RegisAlencar <regis.alencar.cunha@gmail.com>
2021-07-23 22:17:53 -0300

    quinzena1

--- quinzena1/lista-logica/out/exercicios.js
+++ quinzena1/lista-logica/out/exercicios.js
@@ -51,7 +51,7 @@ const nome = prompt("Digite aqui o seu nome!")
 const idade = prompt("Digite a sua idade!")
 const email = prompt("Digite o seu email!")
 
-console.log("Meu nome é", (nome),", tenho",(idade), " anos, e o meu email é ", (email))
+console.log("Meu nome é", nome, "tenho", idade, "anos, e o meu email é", email)
 }
 
 // Exercício 5
@@ -60,7 +60,9 @@ const primeiraCor = prompt("Digite sua cor favorita.")
 const segundaCor = prompt("Digite sua segunda cor favorita.")
 const terceiraCor = prompt("Digite sua terceira cor favorita.")
 
-console.log(primeiraCor),", ", (segundaCor), ", ", (terceiraCor)
+arrayCores = [primeiraCor, segundaCor, terceiraCor]
+
+console.log(arrayCores)
 }
 
 // Exercício 6
@@ -80,12 +82,24 @@ function calculaIngressosEspetaculo() {
 
 // Exercício 8
 function checaStringsMesmoTamanho() {
-  // escreva seu código aqui
+  const primeiraPalavra = prompt("Digite uma palavra")
+  const segundaPalavra = prompt("Digite outra palavra")
+  
+  resultado = primeiraPalavra.length === segundaPalavra.length
+  
+  console.log(resultado)
 }
 
 // Exercício 9
 function checaIgualdadeDesconsiderandoCase() {
-  // escreva seu código aqui
+  const primeiraPalavra = prompt("Digite uma palavra")
+  const segundaPalavra = prompt("Digite outra palavra")
+  
+  fraseIgual1 = primeiraPalavra.toLowerCase()
+  fraseIgual2 = segundaPalavra.toLowerCase()
+  resultado = fraseIgual1 === fraseIgual2
+  
+  console.log(resultado)
 }
 
 // Exercício 10
@@ -93,14 +107,49 @@ function checaRenovacaoRG() {
   const anoAtual = prompt("Olá, digite o ano atual!")
   const anoNascimento = prompt("Digite agora o seu ano de nascimento.")
   const anoId = prompt("Digite aqui a data de expedição da sua Identidade.")
+
+  idade = anoAtual - anoNascimento
+  validadeRg = anoAtual - anoId
+
+  resultado1 = idade <= 20 && validadeRg >= 5
+  resultado2 = idade > 20 && idade <= 50 && validadeRg >= 10
+  resultado3 = idade >= 50 && validadeRg >= 15
+  console.log(resultado1 || resultado2 || resultado3)
 }
+
+
 
 // Exercício 11
 function checaAnoBissexto() {
-  // escreva seu código aqui
-}
+  const entradaAno = Number(prompt("Digite um ano para saber se é Bissexto!"))
+ 
+  anoBissexto = entradaAno % 400 === 0 === true || entradaAno % 4 === 0 === true && entradaAno % 100 === 0 === false 
+  
+  
+  console.log(anoBissexto)
+  
+} 
+
 
 // Exercício 12
 function checaValidadeInscricaoLabenu() {
-  // escreva seu código aqui
+ 
+  const idade = prompt("Voce é maior de 18 anos?")
+  const ensinoMedio = prompt("Possui ensino médio completo?")
+  const disponibilidade = prompt("Possui disponibilidade exclusiva durante os horários do curso?")
+  
+  maiorIdade = idade === "sim"
+  ensinoCompleto = ensinoMedio === "sim"
+  horarioAprovado = disponibilidade === "sim"
+  
+  aprovado = maiorIdade && ensinoCompleto && horarioAprovado
+  
+  console.log(aprovado)
 }
+
+
+
+
+
+
+
