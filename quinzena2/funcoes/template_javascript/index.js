61df08ffaae9e805fdce97661eb6e2fcd7010d06
RegisAlencar <regis.alencar.cunha@gmail.com>
2021-07-27 22:03:40 -0300

    funcoes

---
+++ quinzena2/funcoes/template_javascript/index.js
@@ -0,0 +1,101 @@
+// Exercícios de interpretação de código:
+// 1) 
+// a: Será impresso no console 10, 50.
+// b: Não aparecerá nada.
+
+/*2)
+a: Pedirá para o usúario inserir um texto, em seguida irá deixar todas as letras
+minusculas e irá determinar um boleano na frase sendo verdadeiro ou falso com a palavra "cenoura".
+
+b:  i. eu gosto de cenoura - true
+   ii. cenoura é bom pra vista - true
+  iii. cenouras crescem na terra - false
+
+  Exercícios de escrita de código:
+  1) a: */
+
+//   function informacoesPessoais() {
+//       console.log("Eu sou o Regis, tenho 28 anos, moro em São Paulo e sou desenvolvedor")
+//   }
+// b:
+// function dados(nome, idade, cidade, profissao) {
+
+//      nome2 = ("Regis")
+//      idade2 = ("28anos")
+//      cidade2 = ("Guarulhos")
+//      profissao2 = ("Desenvolvedor")
+// console.log("Eu me chamo", (nome2), "tenho", (idade2), "moro em", (cidade2), "e sou", (profissao2))
+// }
+
+// dados()
+
+// 2)
+// a:
+// function doisNumeros(num1, num2) {
+// const soma = num1 + num2
+// console.log(soma)
+// }
+    
+// doisNumeros(2,2)
+
+// b: 
+// function doisNumeros(num1, num2) {
+// const resultado = num1 >= num2
+// console.log(resultado)
+// }
+    
+// doisNumeros(2,2)
+
+// c: 
+// function doisNumeros() {
+// const num1 = 2 % 2 === 0
+// console.log(num1)
+// }
+    
+// doisNumeros()
+
+// d:
+// function palavra(string){
+// const string = "Olá mundo"
+
+// console.log(string.toUpperCase)
+
+// }
+// palavra()
+
+// 3:
+// function soma(num1, num2) {
+//     const resultado1 = num1 + num2
+// console.log(resultado1)
+// }
+
+// function subtracao(num1, num2) {
+//     const resultado2 = num1 - num2
+//     console.log(resultado2)
+// }
+
+// function multiplicacao(num1, num2) {
+//     const resultado3 = num1 * num2
+//     console.log(resultado3)
+// }
+
+// function divisao(num1, num2) {
+// const resultado4 = num1 / num2
+// console.log(resultado4)
+// }
+
+// const primeiroNumero = prompt("Digite 1 numero")
+// const segundoNumero = prompt("Digite outro numero")
+
+// soma(primeiroNumero, segundoNumero)
+// subtracao(primeiroNumero, segundoNumero)
+// multiplicacao(primeiroNumero, segundoNumero)
+// divisao(primeiroNumero, segundoNumero)
+
+
+
+
+
+
+
+
