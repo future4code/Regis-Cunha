c97db7af22cf0e63bca85e766b7b535279dc82ef
RegisAlencar <regis.alencar.cunha@gmail.com>
2021-07-29 17:00:17 -0300

    objetos

---
+++ quinzena2/objetos/index.js
@@ -0,0 +1,78 @@
+// Exercícios de interpretação de código
+//1) a: Matheus Nachtergaele
+ //  Virginia Cavendish
+ //  Canal Globo, horario: 14h
+
+//  2) a: nome: "Juca", idade: 3, raca: "SRD".
+// nome: "Juba", idade: 3, raca: "SRD".
+// nome: "Jubo", idade: 3, raca: "SRD".
+
+//  b: A sinxtaxe dos três pontos tem como objetivo adcionar algo relevante no objeto.
+
+// 3) a: false, undefined
+// b: O primeiro console.log imprimiu dentro do objeto pessoa o "backender".
+// Já no segundo console.log, ele buscou a "altura", resultando em undefined por não
+// existir a propriedade mencionada.
+
+
+// Exercícios de escrita de código
+// 1) a:
+// const pessoa = {
+//     nome: "Regis",
+//     apelidos: ["reginho, re, ramocles"]
+// }
+
+// console.log(`Eu sou ${pessoa.nome}, mas pode me chamar de ${pessoa.apelidos}.`)
+
+// b:
+// const pessoa = {
+//     nome: "Regis",
+//     apelidos: ["reginho, re, ramocles"]
+// }
+// const novosApelidos = { ... pessoa, apelidos: "ginho, galã, amigão"
+// }
+
+// console.log(novosApelidos)
+
+// 2)
+
+
+// const usuario1 = {
+//     nome: "Regis",
+//     idade: 28,
+//     profissao: "desenvolvedor"
+// }
+
+// const usuario2 = {
+//     nome: "Renata",
+//     idade: 33,
+//     profissao: "sushiman"
+// }
+
+// console.log(usuario1.nome, usuario1.nome.length, usuario1.idade, usuario1.profissao,
+//     usuario1.profissao.length) 
+
+// console.log(usuario2.nome, usuario2.nome.length, usuario2.idade, usuario2.profissao,
+//     usuario2.profissao.length)
+
+
+// 3)
+
+
+
+// const fruta1 = { nome: "melancia", disponibilidade: true}
+// const fruta2 = { nome: "abacaxi", disponibilidade: true}
+// const fruta3 = { nome: "manga", disponibilidade: false}
+
+// carrinho = [fruta1, fruta2, fruta3]
+
+
+// console.log(carrinho)
+
+
+
+
+
+
+
+
