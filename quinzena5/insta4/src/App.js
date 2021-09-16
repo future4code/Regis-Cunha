dc6d8b4abc66d845e3db70352d29c2fa6d3e8f37
RegisAlencar <regis.alencar.cunha@gmail.com>
2021-09-16 13:33:55 -0300

    insta4

---
+++ quinzena5/insta4/src/App.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <MainContainer>
+        <Post
+          nomeUsuario={'Paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+        />
+
+        <Post nomeUsuario={'Regis'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+          />
+
+       <Post nomeUsuario={'Teresa'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'}
+          /> 
+
+      </MainContainer>
+       
+      
+      
+    );
+  }
+}
+
+export default App;
+
+
